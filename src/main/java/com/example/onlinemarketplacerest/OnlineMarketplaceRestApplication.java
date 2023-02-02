c93565f1ba2048737ac0baa2d3dac31a395e9b82
Maksim Ganin <ganin.m479@gmail.com>
2023-02-02 16:04:30 +0400

    Init commit.

---
+++ src/main/java/com/example/onlinemarketplacerest/OnlineMarketplaceRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinemarketplacerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMarketplaceRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineMarketplaceRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onlinemarketplacerest/OnlineMarketplaceRestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.onlinemarketplacerest;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMarketplaceRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
